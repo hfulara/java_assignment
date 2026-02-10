7f54d07ade144fb077da06491f8a4c48a7a038bc
Himanshu Fulara <himanshu.fulara@gmail.com>
2026-02-10 20:58:54 +0530

    Updated LocationGateway scope to ApplicationScoped

--- src/main/java/com/fulfilment/application/monolith/location/LocationGateway.java
+++ src/main/java/com/fulfilment/application/monolith/location/LocationGateway.java
@@ -2,10 +2,13 @@ package com.fulfilment.application.monolith.location;
 
 import com.fulfilment.application.monolith.warehouses.domain.models.Location;
 import com.fulfilment.application.monolith.warehouses.domain.ports.LocationResolver;
+import jakarta.enterprise.context.ApplicationScoped;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@ApplicationScoped
 public class LocationGateway implements LocationResolver {
 
   private static final List<Location> locations = new ArrayList<>();
